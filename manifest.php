7d1d6c3f710046f5c0608806b5d7bf5ab0cdeace
moldcraft <moldcraft@email.com>
2014-11-27 09:43:42 +0200

    Added the update parameter

--- manifest.php
+++ manifest.php
@@ -6,9 +6,7 @@ $manifest = array();
 
 $manifest['name']          = __( 'Page Builder', 'fw' );
 $manifest['description']   = __( "Lets you easily build countless pages with the help of the drag and drop visual page builder that comes with a lot of already created shortcodes.", 'fw' );
-$manifest['version']       = '0.0.0';
-$manifest['author']        = 'ThemeFuse';
-$manifest['author_uri']    = 'http://themefuse.com/';
+$manifest['version']       = '1.0.0';
 $manifest['display']       = true;
 $manifest['standalone']    = true;
 $manifest['requirements']  = array(
@@ -16,3 +14,5 @@ $manifest['requirements']  = array(
 		'builder' => array(),
 	),
 );
+
+$manifest['github_update'] = 'ThemeFuse/Unyson-PageBuilder-Extension';
