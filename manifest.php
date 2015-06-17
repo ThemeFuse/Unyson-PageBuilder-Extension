956ce24b1c2525b7a8a2a083286e38edbc5f9b0e
moldcraft <moldcraft@email.com>
2015-06-17 14:15:04 +0300

    Version 1.3.9

--- manifest.php
+++ manifest.php
@@ -6,12 +6,14 @@ $manifest = array();
 
 $manifest['name']          = __( 'Page Builder', 'fw' );
 $manifest['description']   = __( "Lets you easily build countless pages with the help of the drag and drop visual page builder that comes with a lot of already created shortcodes.", 'fw' );
-$manifest['version']       = '1.3.8';
+$manifest['version']       = '1.3.9';
 $manifest['display']       = true;
 $manifest['standalone']    = true;
 $manifest['requirements']  = array(
 	'framework' => array(
-		// Requires Unyson minimum version 2.1.18, as in that version was solved the bug with children extension requirements when activate an extension
+		/**
+		 * In that version was solved the bug with children extension requirements when activate an extension
+		 */
 		'min_version' => '2.1.18',
 	),
 	'extensions' => array(
