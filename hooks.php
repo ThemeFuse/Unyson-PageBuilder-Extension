39823637f72abcf6f1b1d669f889b5eff4c588d3
moldcraft <moldcraft@email.com>
2015-06-23 16:56:24 +0300

    fixes https://github.com/ThemeFuse/Unyson/issues/690

--- hooks.php
+++ hooks.php
@@ -3,14 +3,29 @@
 }
 
 /**
- * Registers support of feedback for post types checked in dashboard.
+ * Call add_post_type_support('{post-type}', 'fw-page-builder')
+ * for post types checked on Page Builder Settings page.
  */
-function fw_ext_page_builder_add_support()
-{
-	$post_types   = fw_get_db_ext_settings_option('page-builder', 'post_types');
+function _action_fw_ext_page_builder_add_support() {
 	$feature_name = fw_ext('page-builder')->get_supports_feature_name();
-	foreach ($post_types as $slug => $value) {
+
+	foreach (
+		array_keys(fw_get_db_ext_settings_option('page-builder', 'post_types', array()))
+		as $slug
+	) {
 		add_post_type_support($slug, $feature_name);
 	}
 }
-add_action( 'init', 'fw_ext_page_builder_add_support' );
+add_action( 'init', '_action_fw_ext_page_builder_add_support',
+	/**
+	 * Call this as late as possible to make sure all post types were registered.
+	 *
+	 * Calling this earlier, will cause some post types to not appear in the checkboxes list on Page Builder Settings page.
+	 * That happens when fw_get_db_ext_settings_option('page-builder', ...) is called,
+	 * there are no values in db and settings options are extracted from settings options array.
+	 * In settings options is used fw_ext_page_builder_get_supported_post_types() which returns the registered post types,
+	 * and because it will be called earlier than other post types has been registered,
+	 * those post types will not be available.
+	 */
+	9999
+);
