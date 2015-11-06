948cf3ae07e334504b68be6e2559e5fca43de4ec
moldcraft <moldcraft@email.com>
2015-11-06 12:03:10 +0200

    fixes https://github.com/ThemeFuse/Unyson/issues/1000

--- settings-options.php
+++ settings-options.php
@@ -11,8 +11,9 @@ $options = array(
 				'label'   => __( 'Activate for', 'fw' ),
 				'type'    => 'checkboxes',
 				'choices' => fw_ext_page_builder_get_supported_post_types(),
-				'value'   => array(
-					'page' => true
+				'value'   => apply_filters(
+					'fw_ext_page_builder_settings_options_post_types_default_value', 
+					array( 'page' => true )
 				),
 				'desc'    => __( 'Select the posts you want the Page Builder extension to be activated for', 'fw' )
 			),
