431fae2a02a29fd9fddf0b725127c06b203e20dd
moldcraft <moldcraft@email.com>
2015-11-20 19:39:55 +0200

    Version 1.4.4

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $manifest['description']   = __(
 	.' that comes with a lot of already created shortcodes.',
 	'fw'
 );
-$manifest['version']       = '1.4.3';
+$manifest['version']       = '1.4.4';
 $manifest['display']       = true;
 $manifest['standalone']    = true;
 $manifest['requirements']  = array(
