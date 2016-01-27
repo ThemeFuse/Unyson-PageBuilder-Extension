1b19d0ec9a260e063d91ee67c1776417b3672ca9
moldcraft <moldcraft@email.com>
2016-01-27 15:10:01 +0200

    testing https://github.com/ThemeFuse/Unyson/issues/838#issuecomment-175592276

--- helpers.php
+++ helpers.php
@@ -13,14 +13,24 @@ function fw_ext_page_builder_is_builder_post($post_id = '')
  */
 function fw_ext_page_builder_get_supported_post_types()
 {
-	$post_types = get_post_types(array('public' => true), 'objects');
+	$cache_key = fw()->extensions->get('page-builder')->get_cache_key('/supported_post_types');
 
-	$result = array();
-	foreach ($post_types as $key => $post_type) {
-		if (post_type_supports($key, 'editor')) {
-			$result[$key] = $post_type->labels->name;
+	try {
+		return FW_Cache::get($cache_key);
+	} catch (FW_Cache_Not_Found_Exception $e) {
+		$post_types = get_post_types(array('public' => true), 'objects');
+
+		$result = array();
+		foreach ($post_types as $key => $post_type) {
+			if (post_type_supports($key, 'editor')) {
+				$result[$key] = $post_type->labels->name;
+			}
 		}
-	}
 
-	return apply_filters('fw_ext_page_builder_supported_post_types', $result);
+		$result = apply_filters('fw_ext_page_builder_supported_post_types', $result);
+
+		FW_Cache::set($cache_key, $result);
+
+		return $result;
+	}
 }
