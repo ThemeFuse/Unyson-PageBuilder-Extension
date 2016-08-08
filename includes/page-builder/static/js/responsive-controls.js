647c6cc0d0a44fdc17c591b54d8b7b8924a0fd00
moldcraft <moldcraft@email.com>
2016-08-08 18:02:00 +0300

    fixed indentation

--- includes/page-builder/static/js/responsive-controls.js
+++ includes/page-builder/static/js/responsive-controls.js
@@ -1,79 +1,78 @@
-(function($, itemData) {
+(function ($, itemData) {
+	var responsive_class = 'fw-responsive-controls',
+		display_class = 'fw-display-controls',
+		allowed_width = 280;
 
-    var responsive_class = 'fw-responsive-controls',
-	display_class = 'fw-display-controls',
-	allowed_width = 280;
-    
-    $('.fw-option-type-page-builder input[type=hidden]:first').on('change', function() {
-	calculateSize();
-    });
-
-    $(window).resize(function() {
-	calculateSize();
-    });
-
-    /**
-     * Add responsive-class if width is less than allowed.
-     */
-    function calculateSize() {
-	$('.fw-option-type-page-builder .builder-root-items .pb-item').each(function() {
-	    var element = jQuery(this);
-	    var item = element.closest('.builder-item');
-	    if ( allowed_width >= element.width() ) {
-		item.addClass( responsive_class );
-	    } else {
-		item.removeClass( responsive_class );
-		item.removeClass( display_class );
-	    }
-	    
-	    var controls = element.find( '.controls:first' );
-	    controls.mouseenter( function() {
-		item.addClass( display_class );
-	    } );
-	    
-	    controls.mouseleave( function() {
-		item.removeClass( display_class );
-	    } );
+	$('.fw-option-type-page-builder input[type=hidden]:first').on('change', function () {
+		calculateSize();
 	});
-    }
-	
-    /**
-     * Add icon to elements.
-     * @param {Object} data
-     */
-    function addIcon( data ) {
-	data.$controls.append(
-	    $('<i class="fw-responsive-button dashicons dashicons-menu"></i>')
-	    .attr('data-hover-tip', itemData.l10n.responsive)
-	    .on('click', function(e) {
-		e.stopPropagation();
-		e.preventDefault();
-	    })
-	);
-    }
-    
-    fwEvents.on('fw-builder:page-builder:items-loaded', function() {
-	setTimeout( _.partial( calculateSize ), 250 );
-    });
-    
-    // Add controls for simple shortcodes.
-    fwEvents.on('fw:page-builder:shortcode:item-simple:controls', function( data ) {
-	addIcon( data );
-    });
-    
-    // Add controls for sections.
-    fwEvents.on('fw:page-builder:shortcode:section:controls', function(data) {
-	addIcon( data );
-    });
 
-    // Add controls for columns.
-    fwEvents.on('fw:page-builder:shortcode:column:controls', function(data) {
-	addIcon( data );
-    });
-    
-    // Add controls for contact form.
-    fwEvents.on('fw:page-builder:shortcode:contact-form:controls', function(data) {
-	addIcon( data );
-    });
-	
+	$(window).resize(function () {
+		calculateSize();
+	});
+
+	/**
+	 * Add responsive-class if width is less than allowed.
+	 */
+	function calculateSize() {
+		$('.fw-option-type-page-builder .builder-root-items .pb-item').each(function () {
+			var element = jQuery(this);
+			var item = element.closest('.builder-item');
+			if (allowed_width >= element.width()) {
+				item.addClass(responsive_class);
+			} else {
+				item.removeClass(responsive_class);
+				item.removeClass(display_class);
+			}
+
+			var controls = element.find('.controls:first');
+			controls.mouseenter(function () {
+				item.addClass(display_class);
+			});
+
+			controls.mouseleave(function () {
+				item.removeClass(display_class);
+			});
+		});
+	}
+
+	/**
+	 * Add icon to elements.
+	 * @param {Object} data
+	 */
+	function addIcon(data) {
+		data.$controls.append(
+			$('<i class="fw-responsive-button dashicons dashicons-menu"></i>')
+				.attr('data-hover-tip', itemData.l10n.responsive)
+				.on('click', function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+				})
+		);
+	}
+
+	fwEvents.on('fw-builder:page-builder:items-loaded', function () {
+		setTimeout(_.partial(calculateSize), 250);
+	});
+
+	// Add controls for simple shortcodes.
+	fwEvents.on('fw:page-builder:shortcode:item-simple:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for sections.
+	fwEvents.on('fw:page-builder:shortcode:section:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for columns.
+	fwEvents.on('fw:page-builder:shortcode:column:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for contact form.
+	fwEvents.on('fw:page-builder:shortcode:contact-form:controls', function (data) {
+		addIcon(data);
+	});
+
 })(jQuery, _fw_option_type_page_builder_shortcodes_controls);
--- includes/page-builder/static/js/shortcodes-eye.js
+++ includes/page-builder/static/js/shortcodes-eye.js
@@ -1,82 +1,79 @@
-(function( $, itemData ) {
-    
-    var visibleIcon = 'dashicons-visibility',
-	hiddenIcon = 'dashicons-hidden';
-    
-    /**
-     * Change model visibility attribute and shortcode class.
-     * @param {Object} model
-     */
-    function visibilitySettings( model ) {
-	var visibility = model.get( itemData.key );
-	var element = $(model.view.el);
+(function ($, itemData) {
+	var visibleIcon = 'dashicons-visibility',
+		hiddenIcon = 'dashicons-hidden';
 
-	if( visibility || 'undefined' === typeof( visibility ) ) {
-	    element.addClass('fw-visibility-off');
-	    model.set( itemData.key, false );
-	} else {
-	    element.removeClass( 'fw-visibility-off' );
-	    model.set( itemData.key, true );
+	/**
+	 * Change model visibility attribute and shortcode class.
+	 * @param {Object} model
+	 */
+	function visibilitySettings(model) {
+		var visibility = model.get(itemData.key);
+
+		if (visibility || 'undefined' === typeof( visibility )) {
+			model.view.$el.addClass('fw-visibility-off');
+			model.set(itemData.key, false);
+		} else {
+			model.view.$el.removeClass('fw-visibility-off');
+			model.set(itemData.key, true);
+		}
 	}
-    }
-    
-    /**
-     * Change dashicons and shortcode visibility class.
-     * @param {Object} model
-     */
-    function displayIcon( model ) {
-	var visibility = model.get( itemData.key );
-	var element = $(model.view.el);
-	var icon = element.find('.fw-shortcode-visibility:first');
-	
-	if( visibility || 'undefined' === typeof( visibility ) ) {
-	    element.removeClass('fw-visibility-off');
-	    icon.removeClass( hiddenIcon );
-	    icon.addClass( visibleIcon );
-	} else {
-	    element.addClass('fw-visibility-off');
-	    icon.removeClass( visibleIcon );
-	    icon.addClass( hiddenIcon );
+
+	/**
+	 * Change dashicons and shortcode visibility class.
+	 * @param {Object} model
+	 */
+	function displayIcon(model) {
+		var visibility = model.get(itemData.key);
+		var icon = model.view.$el.find('.fw-shortcode-visibility:first');
+
+		if (visibility || 'undefined' === typeof( visibility )) {
+			model.view.$el.removeClass('fw-visibility-off');
+			icon.removeClass(hiddenIcon);
+			icon.addClass(visibleIcon);
+		} else {
+			model.view.$el.addClass('fw-visibility-off');
+			icon.removeClass(visibleIcon);
+			icon.addClass(hiddenIcon);
+		}
 	}
-    }
-    
-    /**
-     * Add eye icon to elements.
-     * @param {Object} data
-     */
-    function addIcon( data ) {
-	data.$controls.prepend(
-	    $('<i class="fw-shortcode-visibility dashicons dashicons-visibility"></i>')
-	    .attr('data-hover-tip', itemData.l10n.eye)
-	    .on('click', function(e) {
-		e.stopPropagation();
-		e.preventDefault();
-		
-		visibilitySettings( data.model );
-	    })
-	);
 
-	displayIcon( data.model )
-    }
-    
-    // Add controls for simple shortcodes.
-    fwEvents.on('fw:page-builder:shortcode:item-simple:controls', function( data ) {
-	addIcon( data );
-    });
-    
-    // Add controls for sections.
-    fwEvents.on('fw:page-builder:shortcode:section:controls', function(data) {
-	addIcon( data );
-    });
+	/**
+	 * Add eye icon to elements.
+	 * @param {Object} data
+	 */
+	function addIcon(data) {
+		data.$controls.prepend(
+			$('<i class="fw-shortcode-visibility dashicons dashicons-visibility"></i>')
+				.attr('data-hover-tip', itemData.l10n.eye)
+				.on('click', function (e) {
+					e.stopPropagation();
+					e.preventDefault();
 
-    // Add controls for columns.
-    fwEvents.on('fw:page-builder:shortcode:column:controls', function(data) {
-	addIcon( data );
-    });
-    
-    // Add controls for contact form.
-    fwEvents.on('fw:page-builder:shortcode:contact-form:controls', function(data) {
-	addIcon( data );
-    });
-    
-})( jQuery, _fw_option_type_page_builder_shortcodes_controls );
+					visibilitySettings(data.model);
+				})
+		);
+
+		displayIcon(data.model)
+	}
+
+	// Add controls for simple shortcodes.
+	fwEvents.on('fw:page-builder:shortcode:item-simple:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for sections.
+	fwEvents.on('fw:page-builder:shortcode:section:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for columns.
+	fwEvents.on('fw:page-builder:shortcode:column:controls', function (data) {
+		addIcon(data);
+	});
+
+	// Add controls for contact form.
+	fwEvents.on('fw:page-builder:shortcode:contact-form:controls', function (data) {
+		addIcon(data);
+	});
+
+})(jQuery, _fw_option_type_page_builder_shortcodes_controls);
