021cf8f21211c695d4099089603bf253a08fa000
mowshon <mowshon@yahoo.com>
2016-09-08 17:35:38 +0300

    Move get_builder_data into shortcodes extension.

--- includes/page-builder/includes/item-types/simple/class-page-builder-simple-item.php
+++ includes/page-builder/includes/item-types/simple/class-page-builder-simple-item.php
@@ -3,17 +3,10 @@
 class Page_Builder_Simple_Item extends Page_Builder_Item
 {
 	private $type = 'simple';
-	private $builder_data = array();
 
 	public function get_type()
 	{
 		return $this->type;
-	}
-
-	public function _init() {
-		add_filter( 'fw_ext:shortcodes:collect_shortcodes_data', array(
-			$this, 'add_simple_shortcodes_data_to_filter'
-		) );
 	}
 
 	public function enqueue_static()
@@ -37,14 +30,10 @@ class Page_Builder_Simple_Item extends Page_Builder_Item
 		wp_localize_script(
 			$this->get_builder_type() . '_item_type_' . $this->get_type(),
 			str_replace('-', '_', $this->get_builder_type()) . '_item_type_' . $this->get_type() . '_data',
-			$this->get_builder_data()
+			fw_ext('shortcodes')->get_builder_data()
 		);
 
 		do_action('fw:ext:page-builder:item-type:simple:enqueue_static');
-	}
-
-	public function add_simple_shortcodes_data_to_filter( $structure ) {
-		return array_merge( $structure, $this->get_builder_data() );
 	}
 
 	/**
@@ -69,7 +58,7 @@ class Page_Builder_Simple_Item extends Page_Builder_Item
 	 */
 	protected function get_thumbnails_data()
 	{
-		$data = $this->get_builder_data();
+		$data = fw_ext('shortcodes')->get_builder_data();
 		$thumb_data = array();
 		foreach ($data as $id => $item) {
 			$thumb_data[$id] = array(
@@ -88,91 +77,6 @@ class Page_Builder_Simple_Item extends Page_Builder_Item
 
 		$this->sort_thumbnails($thumb_data);
 		return $thumb_data;
-	}
-
-	private function get_builder_data()
-	{
-		if (empty($this->builder_data)) {
-			$shortcodes = fw()->extensions->get('shortcodes')->get_shortcodes();
-			foreach ($shortcodes as $tag => $shortcode) {
-				$config = $shortcode->get_config('page_builder');
-				if ($config) {
-
-					// check if the shortcode type is valid
-					$config = array_merge(array('type' => $this->type), $config);
-					if ($config['type'] !== $this->get_type()) {
-						continue;
-					}
-
-					if (!isset($config['tab'])) {
-						trigger_error(
-							sprintf(__("No Page Builder tab specified for shortcode: %s", 'fw'), $tag),
-							E_USER_WARNING
-						);
-					}
-
-					$item_data = array_merge(
-						array(
-							'tab'         => '~',
-							'title'       => $tag,
-							'description' => '',
-							'localize' => array(
-								'edit' => __( 'Edit', 'fw' ),
-								'remove' => __( 'Remove', 'fw' ),
-								'duplicate' => __( 'Duplicate', 'fw' ),
-							),
-							'icon' => null,
-							'title_template' => null,
-						),
-						$config
-					);
-
-					if (
-						!isset($item_data['icon'])
-						&&
-						($icon = $shortcode->locate_URI('/static/img/page_builder.png'))
-					) {
-						$item_data['icon'] = $icon;
-					}
-
-					// if the shortcode has options we store them and then they are passed to the modal
-					$options = $shortcode->get_options();
-					if ($options) {
-						$item_data['options'] = $this->transform_options($options);
-						fw()->backend->enqueue_options_static($options);
-					}
-
-					$this->builder_data[$tag] = $item_data;
-				}
-			}
-		}
-
-		return $this->builder_data;
-	}
-
-	/*
-	 * Puts each option into a separate array
-	 * to keep it's order inside the modal dialog
-	 */
-	private function transform_options($options)
-	{
-		$new_options = array();
-		foreach ($options as $id => $option) {
-			if (is_int($id)) {
-				/**
-				 * this happens when in options array are loaded external options using fw()->theme->get_options()
-				 * and the array looks like this
-				 * array(
-				 *    'hello' => array('type' => 'text'), // this has string key
-				 *    array('hi' => array('type' => 'text')) // this has int key
-				 * )
-				 */
-				$new_options[] = $option;
-			} else {
-				$new_options[] = array($id => $option);
-			}
-		}
-		return $new_options;
 	}
 
 	/*
@@ -195,7 +99,7 @@ class Page_Builder_Simple_Item extends Page_Builder_Item
 		unset($attributes['_items']);
 
 		if (
-			($builder_data = $this->get_builder_data())
+			($builder_data = fw_ext('shortcodes')->get_builder_data())
 			&&
 			isset($builder_data[ $attributes['shortcode'] ])
 			&&
@@ -243,11 +147,6 @@ class Page_Builder_Simple_Item extends Page_Builder_Item
 
 		return $return;
 	}
-}
-
-if (defined('DOING_AJAX')) {
-	$simple = new Page_Builder_Simple_Item();
-	$simple->_init();
 }
 
 FW_Option_Type_Builder::register_item_type('Page_Builder_Simple_Item');
