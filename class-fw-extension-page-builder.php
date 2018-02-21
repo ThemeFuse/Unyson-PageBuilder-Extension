85d554260d973b5b0eb8bc34b1f022ea747ce66b
Colin Duwe <colinduwe@gmail.com>
2018-02-21 10:50:54 -0800

    replaced original data santization with wp_kses_post() as requested.

--- class-fw-extension-page-builder.php
+++ class-fw-extension-page-builder.php
@@ -211,10 +211,7 @@ class FW_Extension_Page_Builder extends FW_Extension {
 			$post_content = str_replace('\\', '\\\\', $post_content); // WordPress "fixes" the slashes
 			$post_content = do_shortcode($post_content);
 			$post_content = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $post_content);
-			$post_content = strip_tags($post_content, '<a><p><h1><h2><h3><h4><h5><h6><img>');
-			$post_content = trim($post_content);
-			$post_content = implode("\n", array_filter(explode("\n", $post_content), 'trim')); // remove extra \n
-			$post_content = normalize_whitespace($post_content);
+			$post_content = wp_kses_post( $post_content );
 
 			// In case some shortcode option has been changed but it doesn't have impact on html
 			$post_content .= "\n\n". '<!-- '. md5($builder_data['json']) .' -->';
